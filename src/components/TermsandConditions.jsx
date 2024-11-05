d5170e43a1c95053dcf3216a75960d32c33b4d32
Eithan <eithanmendoza876@gmail.com>
2024-11-04 23:02:19 -0600

    gabo3

--- src/components/TermsandConditions.jsx
+++ src/components/TermsandConditions.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../controllers/navbarT';
-import Footer from '../controllers/Footer';
+import Footer from '../controllers/footer';
 
 const TermsAndConditions = () => {
   return (
